f9ab19ed6ebc18371cd18150d296bae9428d9b45
SoEnguy <enguerran.henner@gmail.com>
2013-12-19 16:17:58 +0100

    Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BeCoach::Application.routes.draw do
+  resources :challenges
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,8 @@ BeCoach::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+
+  root :to => 'challenges#index'
 
   # See how all your routes lay out with "rake routes"
 

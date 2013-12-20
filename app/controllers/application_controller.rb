069439dfb81dac1cd2f707986f76b77897fd6175
Julien Kosinski <julien.kosinski@gmail.com>
2013-12-20 11:19:27 +0100

    Attempt to fix env production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :require_login
+  def default_url_options
+    if Rails.env.production?
+      {:host => "be-coach.herokuapp.com"}
+    else  
+      {}
+    end
+  end
 end

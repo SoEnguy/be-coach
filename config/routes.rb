ee7a4ad4452a8378c6bdf11bc5627b4a50c1e3d9
SoEnguy <enguerran.henner@gmail.com>
2014-01-15 16:38:17 +0100

    Added FAQ static page + facebook connect on button

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ BeCoach::Application.routes.draw do
   get "oauths/callback" => "oauths#callback" # for use with Github
   get "oauths/:provider" => "oauths#oauth", :as => :auth_at_provider
 
+  get "/FAQ" => 'welcome#FAQ'
   root :to => 'welcome#index'
 end

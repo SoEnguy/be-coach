e8b0137e1f13ca189cd9e9a885caf18b2e6ab76f
ABattut <arthur.battut@gmail.com>
2014-01-16 15:37:44 +0100

    Add logout button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140116142004) do
+ActiveRecord::Schema.define(:version => 20140102122529) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",    :null => false

25cbce3a8240292cc112bb570fa8b9a984a21200
SoEnguy <enguerran.henner@gmail.com>
2014-01-16 15:29:54 +0100

    Fixed table link_steps_users

--- db/migrate/20131227174628_create_link_steps_users.rb
+++ db/migrate/20131227174628_create_link_steps_users.rb
@@ -1,9 +1,10 @@
 class CreateLinkStepsUsers < ActiveRecord::Migration
   def change
-    create_join_table :user, :step, table_name: :link_steps_users do |t|
+    create_table :link_steps_users do |t|
       t.references :user
       t.references :step
       t.references :challenge
+      t.boolean :isComplete
     end
     add_index :link_steps_users, :user_id
     add_index :link_steps_users, :step_id
